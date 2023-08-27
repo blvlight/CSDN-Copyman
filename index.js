030bd9c51c3b91b04990922e5d2c53fd25a7bff3
blvlight <2558968357@qq.com>
2023-08-27 11:28:20 +0800

    feature: add contentEditable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSDN Copyman
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Press S and click to copy without login
 // @author       blvlight
 // @match        https://*.csdn.net/*
@@ -13,14 +13,16 @@
 (function () {
   'use strict';
   console.log('cm is working');
+
+  document.body.contentEditable = true;
+  document.querySelector('article').contentEditable = true;
+
   const key = 's';
   window.addEventListener('keydown', handleKeydown);
   window.addEventListener('keyup', handleKeyUp);
 
   function handleKeydown(e) {
-    if (e.key === key) {
-      window.addEventListener('click', handleClick);
-    }
+    if (e.key === key) window.addEventListener('click', handleClick);
   }
 
   function handleKeyUp(e) {
@@ -29,13 +31,16 @@
 
   /**
    * 
-   * @param {HTMLElement} e 
+   * @param {MouseEvent} e 
    */
   function handleClick(e) {
-    let str = '', q = [e];
-    const texts = e.innerHTML.split(/<.*?>/);
-    texts.reduce((text, cur) => cur + text, str);
-    console.log(`from CSDNcopyman ${str}`);
-    navigator.clipboard.writeText(str).then(() => alert(`复制内容\n${str}`));
+    let str = '';
+    const texts = e.target?.innerHTML.replace(/<.*?>/, '');
+    if (texts) {
+      console.log(`from CSDNcopyman - You can check your copy text in '[]':\n[\n${str}\n]\n----${new Date().toLocaleString()}`);
+      navigator.clipboard.writeText(str).then(() => alert(`复制内容：\n${str}`));
+    } else {
+      console.log(`from CSDNcopyman - It seems like click void\n----${new Date().toLocaleString()}`);
+    }
   }
 })();
